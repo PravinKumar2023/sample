bbec5b5fdf98589a475ac813a771b243bd7820c6
PravinKumar2023 <Pravinips007@gmail.com>
2023-05-01 16:10:12 +0530

    2 methods for RR

--- src/test/java/Sample/GitA.java
+++ src/test/java/Sample/GitA.java
@@ -3,6 +3,18 @@ package Sample;
 public class GitA {
 
 	private void sample() {
+		
+
+	}
+	
+	private void omega() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	private void rolex() {
+		// TODO Auto-generated method stub
 
 	}
 }
